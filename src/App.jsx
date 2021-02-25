b9d4654739be3fa20e0f6474350fd7de5a5d27db
Chetan <chetan.narian@gmail.com>
2021-02-25 13:44:05 +0530

    Added Button and included Button in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,9 @@ import { RendererProvider } from "react-fela";
 
 const renderer = createRenderer();
 
+//* Fela components
+import Button from "./Button";
+
 function App() {
   const [count, setCount] = useState(0);
 
@@ -13,33 +16,9 @@ function App() {
     <RendererProvider renderer={renderer}>
       <div className="App">
         <header className="App-header">
-          <p>
-            <button onClick={() => setCount((count) => count + 1)}>
-              count is: {count}
-            </button>
-          </p>
-          <p>
-            Edit <code>App.jsx</code> and save to test HMR updates.
-          </p>
-          <p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-            {" | "}
-            <a
-              className="App-link"
-              href="https://vitejs.dev/guide/features.html"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Vite Docs
-            </a>
-          </p>
+          <Button onClick={() => setCount((count) => count + 1)}>
+            count is: {count}
+          </Button>
         </header>
       </div>
     </RendererProvider>
--- src/Button.jsx
+++ src/Button.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { useFela } from "react-fela";
 
-function Button({ children, fontSize = 16 }) {
+const Button = ({ children, fontSize = 16 }) => {
   const { css } = useFela();
 
   return (
     <button
       className={css({
-        padding: 10,
+        padding: ".5em",
+        borderRadius: "8px",
         fontSize,
         color: "darkblue",
         ":hover": {
@@ -18,4 +19,6 @@ function Button({ children, fontSize = 16 }) {
       {children}
     </button>
   );
-}
+};
+
+export default Button;
